4c7c68486ea066f7148edd3f6d57584932bc862d
onlyoneaman <2000.aman.sinha@gmail.com>
2024-04-07 13:47:57 +0530

    Add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Home from "@/components/Home/Home.js";
+
 const Link = (props: JSX.IntrinsicElements['a']) => (
   <a
     className="text-pink-500 underline hover:no-underline dark:text-pink-400"
@@ -6,6 +8,11 @@ const Link = (props: JSX.IntrinsicElements['a']) => (
 );
 
 export default function App() {
+
+  return (
+    <Home />
+  )
+
   return (
     <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
       <h1 className="mb-4 text-4xl">Welcome</h1>
