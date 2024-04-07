c5ec2abd6596ff83345c73a2ac77f44fd5f46d6b
onlyoneaman <2000.aman.sinha@gmail.com>
2024-04-07 14:29:02 +0530

    ContactPage Placeholder

--- src/components/ContactPage/ContactPage.tsx
+++ src/components/ContactPage/ContactPage.tsx
@@ -5,6 +5,25 @@ const ContactPage = () => {
   return (
     <Container>
       <h1>Contact Page</h1>
+      <h2>
+        If you're building in / excited about AI or just wanna chat, let's connect.
+      </h2>
+
+      <div>
+        <span>
+          Call Button
+        </span>
+        <span>
+          Email Button
+        </span>
+      </div>
+      <div>
+        Contact Form
+      </div>
+
+      <div>
+        Subscribe to Newsletter
+      </div>
     </Container>
   );
 };
