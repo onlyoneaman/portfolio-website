11402ec136a24469c0f8a20e7ede2f0cf1c6cf0a
onlyoneaman <2000.aman.sinha@gmail.com>
2024-04-07 14:01:50 +0530

    Container

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,9 +1,29 @@
+import Container from "@/components/Layout/Container.js";
+
 const HomePage = () => {
 
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <div>
+        <h1>
+          Hey, I'm Aman
+        </h1>
+        <h2>
+          I love to code
+        </h2>
+
+        <p>
+          A Full-Stack Dev and AI Enthusiast, I have built multiple products in past 4 years, latest being an LLMOps
+          Platform. Raised $100K for Engagebud.
+          I have worked with startups in US, India.
+
+          I help starups and businesses built their web applications and apps. As a Full Stack Developer, I can build you
+          product from scratch or help you with your existing product.
+
+          Talk to me about AI, new ideas, life, or anything else.
+        </p>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/components/Layout/Container.tsx
@@ -0,0 +1,12 @@
+const Container = ({children}) => {
+
+  return (
+    <div
+      className="max-w-xl mx-auto p-4"
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
